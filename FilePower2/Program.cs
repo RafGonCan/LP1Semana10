2488bb09276cc32371ef1f63c60912d18d5fb5d6
Rafael Canhoto <rafgoncan99@gmail.com>
2025-05-09 02:11:14 +0100

    Add FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sentence;
+            using StreamReader senteceWriter = new StreamReader("text.txt");
+
+            while ((sentence = senteceWriter.ReadLine()) != null)
+            {
+                Console.WriteLine(sentence);
+            }
+        }
+    }
+}
