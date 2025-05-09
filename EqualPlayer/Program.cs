3ec3e6876565e2cf56458b92e13ff446dc2bdc51
Rafael Canhoto <rafgoncan99@gmail.com>
2025-05-09 01:22:06 +0100

    Start Program.cs

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -7,9 +7,17 @@ namespace EqualPlayer
     {
         private static void Main(string[] args)
         {
-            HashSet<Player> setOfPlayers = new HashSet<Player> ();
+            HashSet<Player> setOfPlayers = new HashSet<Player> ()
+            {
+                new Player(PLayerClass.Tank, "Ana"),
+                new Player(PLayerClass.Slayer, "Paulo"),
+                new Player(PLayerClass.Tank, "Ana")
+            };
 
-            
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
         }
     }
 }
