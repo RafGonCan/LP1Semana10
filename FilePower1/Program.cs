5a95af6622e4528aa08f86acabf79a29753df8b1
Rafael Canhoto <rafgoncan99@gmail.com>
2025-05-09 02:04:38 +0100

    Add code in Program.cs

---
+++ FilePower1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string fileName = args[0];
+            Queue<string> sentence = new Queue<string> ();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input != "")
+                {
+                    sentence.Enqueue(input);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            File.WriteAllLines(fileName, sentence);
+        }
+    }
+}
